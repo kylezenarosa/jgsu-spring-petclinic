b008cbb16e8ad63cddf76b228438cf03e7500135
Kyle <ksgzenarosa@yahoo.com>
2022-03-28 15:48:16 -0400

    Added sample pipeline file

---
+++ pipeline.groovy
@@ -0,0 +1,24 @@
+pipeline {
+    agent any
+    
+    stages {
+        stage('Checkout') {
+            steps {
+                git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
+                
+            }
+        }
+        
+        stage('Build') {
+            steps {
+                  bat 'mvn package'
+            }
+            post {
+                success {
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                    archiveArtifacts 'target/*.jar'
+                }
+            }
+        }
+    }
+}
