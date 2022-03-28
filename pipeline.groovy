8c2a1d11e1e005f7451048bc4e7f30cf8d34f0e6
Kyle <ksgzenarosa@yahoo.com>
2022-03-28 16:03:01 -0400

    Changed repo url

--- pipeline.groovy
+++ pipeline.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
+                git url: 'https://github.com/kylezenarosa/jgsu-spring-petclinic.git', branch: 'main'
                 
             }
         }
